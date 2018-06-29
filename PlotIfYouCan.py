d8c53f6d456e3d431eeb6874d0585f42363e9f20
kallol <kalloldash@gmail.com>
2018-06-29 17:58:45 -0600

    Plotting different values

---
+++ PlotIfYouCan.py
@@ -0,0 +1,48 @@
+import pickle
+import tensorflow as tf
+from netCDF4 import Dataset
+import pickle
+import numpy as np
+import csv
+import Orange as og
+import math
+import itertools as it
+from Orange.data import Domain, Table
+import random
+from Orange.projection import PCA
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import pandas as pd
+import os
+
+
+#2D Visualizaiton
+def data_visualization_2dr(w_data, title, i=0, visualize=True):
+    if visualize:
+        plt.axis([0, len(w_data[0]), 0, len(w_data)])
+        x, y = w_data.nonzero()
+        # x = range(0, 65)
+        # y = range(0, 44)
+        c = w_data[x, y]
+        plt.scatter(y[:], x[:], c=c[:], cmap='jet')
+        plt.title(title)
+        plt.colorbar()
+        # plt.savefig('com/fig' + str(i) + '.png')
+        plt.show()
+        plt.close()
+
+def display_image(F, **kwargs):
+    plt.figure()
+    plt.imshow(F, **kwargs, origin='lower', cmap='jet')
+    plt.colorbar()
+    plt.show()
+
+#read MAE and RMSE files
+readData = pd.read_csv('25X25/ModelsInfoLR.csv', header=None)
+
+rmse = pd.to_numeric(np.array(readData[13])[1:]).reshape((44, 65))
+print(rmse)
+# data_visualization_2dr(w_data=rmse, title='nothing')
+display_image(rmse)
